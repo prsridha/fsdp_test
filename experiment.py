b74332abb1157371a71b3a89ba6bbd88636314d4
prsridha <prsridha@ucsd.edu>
2023-06-02 14:50:20 -0700

    minor

--- experiment.py
+++ experiment.py
@@ -4,11 +4,11 @@ from mnist import MNISTSpec
 
 def main():
     m = MNISTSpec()
-    datapath = "data"
+    datapath = "../data"
     hyperparams = {
         "lr": m.param_grid["lr"][1]
     }
-    model_path = "models"
+    model_path = "../models"
 
     f = FSDPExecutor(m.train, datapath, hyperparams, model_path)
     f.execute_train()
