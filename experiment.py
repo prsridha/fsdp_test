195403f59e9a6dec0944af162aae38fc2656ddd7
prsridha <prsridha@ucsd.edu>
2023-06-02 17:59:51 -0700

    changed model path

--- experiment.py
+++ experiment.py
@@ -9,7 +9,7 @@ def main():
         "lr": m.param_grid["lr"][1],
         "batch_size": m.param_grid["batch_size"][0]
     }
-    model_path = "../models"
+    model_path = "../models/model1.pt"
 
     f = FSDPExecutor(m.train, datapath, hyperparams, model_path)
     f.execute_train()
